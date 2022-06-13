ecf769afd52d76bb662a8d688dd8f23740f3a636
your Name <mohamedabdulayubm@gmail.com>
2022-06-13 16:00:03 +0530

    Add project files.

---
+++ EmpWage/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Calculate Daily Employee Wage");
+
+
+
+
+int present = 1;
+
+Random num = new Random();
+int ran = num.Next(0, 2);
+
+if (ran == present)
+{
+    Console.WriteLine("Is Present");
+    int empWrkHrs = 8;
+    int empSalary = 50;
+    int empWage = (empWrkHrs * empSalary);
+    Console.WriteLine("The total salary is :" + empWage);
+    
+}
+else
+{
+    Console.WriteLine("Is Absent");
+    int empWrkHrs = 0;
+    int empSalary = 50;
+    int empWage = (empWrkHrs * empSalary);
+    Console.WriteLine("The total salary is :" + empWage);
+
+}
+
+
+
